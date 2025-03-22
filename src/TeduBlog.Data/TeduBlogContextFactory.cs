2c549b0aa733422229df671d3e791143687e9070
DTPhu68 <ptcntt@gmail.com>
2025-03-22 14:22:27 +0700

    2.7. Migrate database version 1

---
+++ src/TeduBlog.Data/TeduBlogContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace TeduBlog.Data
+{
+  public class TeduBlogContextFactory : IDesignTimeDbContextFactory<TeduBlogContext>
+  {
+    public TeduBlogContext CreateDbContext(string[] args)
+    {
+      var configuration = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json")
+           .Build();
+      var builder = new DbContextOptionsBuilder<TeduBlogContext>();
+      builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+      return new TeduBlogContext(builder.Options);
+    }
+  }
+}
