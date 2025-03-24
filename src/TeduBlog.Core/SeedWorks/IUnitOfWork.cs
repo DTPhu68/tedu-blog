91ae3d344a2b55a4f098cb70b8dc50d8343fb59f
DTPhu68 <ptcntt@gmail.com>
2025-03-24 15:26:25 +0700

    Test thử Controller cho Post

--- src/TeduBlog.Core/SeedWorks/IUnitOfWork.cs
+++ src/TeduBlog.Core/SeedWorks/IUnitOfWork.cs
@@ -1,9 +1,10 @@
-﻿using System.Linq.Expressions;
+﻿using TeduBlog.Core.Repositories;
 
 namespace TeduBlog.Core.SeedWorks
 {
   public interface IUnitOfWork
   {
+    IPostRepository Posts { get; }
     Task<int> CompleteAsync();
   }
 }
