c59e5242263a9ce3838974ae43aabbceb83c6cd9
DTPhu68 <ptcntt@gmail.com>
2025-03-24 11:26:53 +0700

    Thử nghiệm repository cho Post

---
+++ src/TeduBlog.Data/Repositories/PostRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeduBlog.Core.Domain.Content;
+using TeduBlog.Core.Repositories;
+using TeduBlog.Data.SeedWorks;
+
+namespace TeduBlog.Data.Repositories
+{
+  public class PostRepository : RepositoryBase<Post, Guid>, IPostRepository
+  {    
+    public PostRepository(TeduBlogContext context) : base(context)
+    {
+    }
+
+    public Task<List<Post>> GetPopularPostsAsync(int count)
+    {
+      return _context.Posts.OrderByDescending(x => x.ViewCount).Take(count).ToListAsync();
+    }
+  }
+}
